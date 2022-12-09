782f5c2fa1805c49966dc7a7528cf2f0c7c89561
Simon Thamer <thamersimon@gmail.com>
2022-12-09 17:00:59 +0100

    day_09_2

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -14,6 +14,7 @@ struct Args {
 
 fn main() -> io::Result<()> {
     challenge_1().unwrap();
+    challenge_2().unwrap();
     Ok(())
 }
 
@@ -67,6 +68,64 @@ fn challenge_1() -> io::Result<()> {
     //let visible = grid.len() * grid[0].len() - hidden;
 
     println!("Points 1:\t{}", visited.len());
+    Ok(())
+}
+
+fn challenge_2() -> io::Result<()> {
+    /*let mut head_pos: (i32, i32) = (0, 0);
+    let mut tail_pos: (i32, i32) = (0, 0);*/
+    let mut knots: Vec<(i32, i32)> = vec![(0, 0); 10];
+    let mut visited = vec![];
+    visited.push((0, 0));
+    let args = Args::parse();
+    // File hosts must exist in current path before this produces output
+    let file = File::open(args.path)?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let c_line = line?;
+        let command = c_line.split(" ").collect::<Vec<&str>>();
+        for _ in 0..command[1].parse::<i32>().unwrap() {
+            match command[0] {
+                "R" => {
+                    knots[0].0 += 1;
+                }
+                "L" => {
+                    knots[0].0 -= 1;
+                }
+                "U" => {
+                    knots[0].1 += 1;
+                }
+                "D" => {
+                    knots[0].1 -= 1;
+                }
+                _ => {
+                    panic!("Invalid command!")
+                }
+            }
+            'inner: for i in 1..10 {
+                if (knots[i].0 == knots[i - 1].0 && (knots[i].1 - knots[i - 1].1).abs() == 1)
+                    || (knots[i].1 == knots[i - 1].1 && (knots[i].0 - knots[i - 1].0).abs() == 1)
+                    || (knots[i].0 - knots[i - 1].0).abs() == 1
+                        && (knots[i].1 - knots[i - 1].1).abs() == 1
+                    || knots[i].0 == knots[i - 1].0 && knots[i].1 == knots[i - 1].1
+                {
+                    // don't move!
+                    continue 'inner;
+                } else {
+                    knots[i].0 += get_direction(knots[i - 1].0 - knots[i].0);
+                    knots[i].1 += get_direction(knots[i - 1].1 - knots[i].1);
+                }
+                if !visited.contains(&knots.last().unwrap()) {
+                    visited.push(knots.last().unwrap().clone());
+                }
+            }
+        }
+    }
+
+    //let visible = grid.len() * grid[0].len() - hidden;
+
+    println!("Points 2:\t{}", visited.len());
     Ok(())
 }
 
