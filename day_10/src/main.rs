f963948ca6280bfc4a042b9f31d3b6d7faf94ec7
Simon Thamer <thamersimon@gmail.com>
2022-12-10 13:03:25 +0100

    optimized day 10

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -18,7 +18,6 @@ struct Args {
 
 fn main() -> io::Result<()> {
     challenge_1_2().unwrap();
-    //challenge_2().unwrap();
     Ok(())
 }
 
@@ -31,36 +30,26 @@ fn challenge_1_2() -> io::Result<()> {
 
     // parse input
     for line in reader.lines() {
-        let c_line = line?;
-        commands.push(Command::new(c_line));
+        commands.push(Command::new(line?));
     }
     let mut iteration_1 = 0;
-    let mut register_1: i32 = 1;
+    let mut iteration_2 = 0;
+    let mut register: i32 = 1;
     let interesting_cycles = vec![20, 60, 100, 140, 180, 220];
     let mut signal_strengths: Vec<i32> = vec![];
 
     // part 1
     for c_cmd in commands.clone() {
         for _ in 0..c_cmd.get_iterations() {
+            //part 1
             iteration_1 += 1;
             if interesting_cycles.contains(&iteration_1) {
-                signal_strengths.push(iteration_1 * register_1);
+                signal_strengths.push(iteration_1 * register);
             }
-        }
-        match c_cmd {
-            Command::Addx(val) => register_1 += val,
-            _ => (),
-        }
-    }
-    println!("Points 1:\t{}", signal_strengths.iter().sum::<i32>());
 
-    // part 2
-    let mut iteration_2 = 0;
-    let mut register_2: i32 = 1;
-    for c_cmd in commands {
-        for _ in 0..c_cmd.get_iterations() {
+            //part 2
             iteration_2 += 1;
-            if (register_2..register_2 + 3).contains(&iteration_2) {
+            if (register..register + 3).contains(&iteration_2) {
                 print!("#");
             } else {
                 print!(".");
@@ -69,15 +58,12 @@ fn challenge_1_2() -> io::Result<()> {
                 iteration_2 = 0;
                 print!("\n");
             }
-            /*if interesting_cycles.contains(&iteration_1) {
-                signal_strengths.push(iteration_1 * register_1);
-            }*/
         }
         match c_cmd {
-            Command::Addx(val) => register_2 += val,
+            Command::Addx(val) => register += val,
             _ => (),
         }
     }
-    //println!("Points 2:\t{}", "");
+    println!("Points 1:\t{}", signal_strengths.iter().sum::<i32>());
     Ok(())
 }
